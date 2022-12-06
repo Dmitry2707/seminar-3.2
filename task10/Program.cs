89bb273b5b93b1c5447493483342e595ab252dc7
Dmitry2707 <kemper1495@gmail.com>
2022-12-06 22:58:34 +0300

    zadanie10

---
+++ task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("введите трехзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringnumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> " + stringnumber[1]);
