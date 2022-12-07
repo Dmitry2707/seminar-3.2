ff0ae051dc77df3fcd11956f3b117d40128b2564
Dmitry2707 <kemper1495@gmail.com>
2022-12-07 23:48:32 +0300

    task15

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,8 +1,7 @@
 ﻿Console.WriteLine("введите число: ");
 string stringnumber = Console.ReadLine();
 int threeDigitNumber = Convert.ToInt32(stringnumber);
-if (stringnumber[2] > 0);
-    Console.WriteLine("третья цифра этого числа -> " + stringnumber[2]); 
+Console.WriteLine("третья цифра этого числа -> " + stringnumber[2]); 
 
 
 
---
+++ task15/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("введите цифру");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+        Console.WriteLine("monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;    
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday - day off");
+        break;
+    case 7:
+        Console.WriteLine("Sunday - day off");
+        break;            
+    
+}
+
+
