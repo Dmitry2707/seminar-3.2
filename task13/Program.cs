caa016c7f7489c57b2595a2766f2fb7798a33572
Dmitry2707 <kemper1495@gmail.com>
2022-12-07 21:16:22 +0300

    task16

---
+++ task13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число: ");
+string stringnumber = Console.ReadLine();
+int threeDigitNumber = Convert.ToInt32(stringnumber);
+if (stringnumber[2] > 0);
+    Console.WriteLine("третья цифра этого числа -> " + stringnumber[2]); 
+
+
+
+
